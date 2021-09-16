1009b1af51e7fd261642c00895ffeb85a6af3eee
Arian Reyhaneh <arian.r.1373@gmail.com>
2021-09-16 18:12:19 +0430

    Add init movement animations

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,27 +10,55 @@ public class PlayerController : MonoBehaviour
     private Vector3 _moveInput;
     private Vector3 _moveVelocity;
 
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
-        {
-            _moveVelocity = Vector3.zero;
-            return;
-        }
-
-        _moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        var horizontalAxisRaw = Input.GetAxisRaw("Horizontal");
+        var verticalAxisRaw = Input.GetAxisRaw("Vertical");
+        
+        AnimationControl(horizontalAxisRaw, verticalAxisRaw);
+        
+        _moveInput = new Vector3(horizontalAxisRaw, 0f, verticalAxisRaw);
         _moveVelocity = _moveInput * moveSpeed;
     }
 
     private void FixedUpdate()
     {
         _rigidBody.velocity = _moveVelocity;
+    }
+
+    private void AnimationControl(float horizontalAxisRaw, float verticalAxisRaw)
+    {
+        if (horizontalAxisRaw == 0 && verticalAxisRaw == 0)
+        {
+            _animator.SetInteger("runningFwd", 0);
+            _animator.SetInteger("runningSideway", 0);
+        } else if (verticalAxisRaw == 0){
+            if (horizontalAxisRaw > 0)
+            {
+                _animator.SetInteger("runningFwd", 0);
+                _animator.SetInteger("runningSideway", 1);
+            }
+            else
+            {
+                _animator.SetInteger("runningFwd", 0);
+                _animator.SetInteger("runningSideway", -1);
+            }
+        } else if (verticalAxisRaw > 0) {
+            _animator.SetInteger("runningFwd", 1);
+            _animator.SetInteger("runningSideway", 0);
+        } else if (verticalAxisRaw < 0) {
+            _animator.SetInteger("runningFwd", -1);
+            _animator.SetInteger("runningSideway", 0);
+        }
     }
 }
