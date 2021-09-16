5004041a8a03e08e7fe6399c93e4f328b55b601c
vahid <vahidghasemi119@gmail.com>
2021-09-16 17:44:15 +0430

    follow mouse position.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed;
 
     private Rigidbody _rigidBody;
+    private Camera _mainCamera;
 
     private Vector3 _moveInput;
     private Vector3 _moveVelocity;
@@ -13,10 +14,36 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovementControl();
+
+        RotationControl();
+    }
+
+    private void RotationControl()
+    {
+        Ray cameraRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(cameraRay, out var rayLength))
+        {
+            Vector3 pointToLock = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(pointToLock.x, transform.position.y, pointToLock.z));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = _moveVelocity;
+    }
+
+    private void MovementControl()
     {
         if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
         {
@@ -26,10 +53,5 @@ public class PlayerController : MonoBehaviour
 
         _moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         _moveVelocity = _moveInput * moveSpeed;
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidBody.velocity = _moveVelocity;
     }
 }
